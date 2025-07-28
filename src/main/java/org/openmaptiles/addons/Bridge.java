4c10a2b3fb678637b12723fec41f547239f799a6
JonasVgt <git@jonasvoigt.de>
2025-07-28 22:51:59 +0200

    add bridge layer

---
+++ src/main/java/org/openmaptiles/addons/Bridge.java
@@ -0,0 +1,40 @@
+package org.openmaptiles.addons;
+
+import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.config.PlanetilerConfig;
+import com.onthegomap.planetiler.reader.SourceFeature;
+import com.onthegomap.planetiler.stats.Stats;
+import com.onthegomap.planetiler.util.Translations;
+import org.openmaptiles.Layer;
+import org.openmaptiles.OpenMapTilesProfile;
+
+public class Bridge implements Layer, OpenMapTilesProfile.OsmAllProcessor {
+
+  private static final String LAYER_NAME = "seamark_bridge";
+
+  Bridge(Translations translations, PlanetilerConfig config, Stats stats) {}
+
+  @Override
+  public String name() {
+    return LAYER_NAME;
+  }
+
+  @Override
+  public void processAllOsm(SourceFeature feature, FeatureCollector features) {
+    if (!feature.hasTag("seamark:type", "bridge"))
+      return;
+
+    String clazz = feature.getString("seamark:bridge:category");
+    String clerance_height = feature.getString("seamark:bridge:clearance_height");
+    if(clerance_height == null)
+      clerance_height = feature.getString("seamark:bridge:clearance_height_closed");
+    String clerance_width = feature.getString("seamark:bridge:clearance_width");
+
+    features.point("seamark_bridge")
+      .setMinZoom(14)
+      .setAttr("class", clazz)
+      .setAttr("clearance_height", clerance_height)
+      .setAttr("clearance_width", clerance_width);
+  }
+
+}
